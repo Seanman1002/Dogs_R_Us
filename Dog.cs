331c08296a20c2614b7b92b816226a3ad2eb56e7
Seanman1002 <gamernerd1002@gmail.com>
2026-06-17 19:01:40 -0500

    chore: move and track missing Dog class file

---
+++ Dog.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Dogs;
+
+public class Dog
+{
+
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public double Weight { get; set; }
+
+   
+
+    public void Bark()
+    {
+        if (Weight < 15)
+            Console.WriteLine("Yip!");
+        else
+        {
+            Console.WriteLine("WOOF!");
+        }
+    }
+}
